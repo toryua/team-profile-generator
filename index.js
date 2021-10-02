ac31eaf4588958bda76e886b42edc5bd5a1ef0b0
toryua <87789457+toryua@users.noreply.github.com>
2021-10-02 12:21:16 -0700

    created parent classes Employee, Engineer, Intern, and Manager

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const 
+const Employee = function(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+}
+
+const Manager = function(name, id, email, officeNumber) {
+    this.name = name
+    this.id = id;
+    this.email = email;
+    this.officeNumber = officeNumber;
+}
+
+const Engineer = function(name, id, email, github) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.github = github;
+}
+
+const Intern = function(name, id, email, school) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.school = school;
+}
+
