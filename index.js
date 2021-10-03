689a4c994c5e9f01e1b7c3031e7f53672ad364d7
toryua <87789457+toryua@users.noreply.github.com>
2021-10-03 16:30:13 -0700

    added classes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// create parent class for Employee
 const Employee = function(name, id, email) {
     this.name = name;
     this.id = id;
     this.email = email;
 }
 
+// create parent class for Manager, same as Employee but with officeNumber
 const Manager = function(name, id, email, officeNumber) {
     this.name = name
     this.id = id;
@@ -11,6 +13,7 @@ const Manager = function(name, id, email, officeNumber) {
     this.officeNumber = officeNumber;
 }
 
+// create parent class for Engineer; same as Employee but with github
 const Engineer = function(name, id, email, github) {
     this.name = name;
     this.id = id;
@@ -18,10 +21,19 @@ const Engineer = function(name, id, email, github) {
     this.github = github;
 }
 
+// create parent class for Intern; same as Employee but with school
 const Intern = function(name, id, email, school) {
     this.name = name;
     this.id = id;
     this.email = email;
     this.school = school;
 }
+
+// create functions
+function getName(name) {
+
+}
+
+// Create an array of questions for user input
+
 
---
+++ tests/employee.test.js
@@ -0,0 +1,13 @@
+const { TestWatchtester } = require('@jest/core');
+const Employee = require('../lib/Employee');
+
+// write a test that checks for existence of name, id, email)
+
+test('creates an employee object', () => {
+    const employee = new Employee('Rachel', 345, 'rachel@rachel.com');
+
+    expect(employee.name).toBe('Rachel');
+    expect(employee.id).toBe(Number);
+    expect(employee.email).toBe(String);
+    
+});
